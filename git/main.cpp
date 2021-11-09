a486ea78b1b96f385eb4e0e1a935466869ff3900
Martin Menchev <menchevmartin@gmail.com>
2021-11-10 01:17:49 +0200

    Initial Commit

---
+++ git/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  git
+//
+//  Created by Martin Menchev on 10.11.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
